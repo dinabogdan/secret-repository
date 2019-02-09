eecc2fcfca58dcf3151023380becb925c614728c
bogdan.dina <bogdan.dina@orange.com>
2019-02-09 17:30:14 +0200

    gradle project

---
+++ event-app/event-app/identity-manager/src/main/java/com/freesoft/identity/manager/IdentityManagerApplication.java
@@ -0,0 +1,11 @@
+package com.freesoft.identity.manager;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityManagerApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
